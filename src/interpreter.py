35e07c8607b53fc68d4e9a7a50d6689ff9709814
ylf <2872444509@qq.com>
2021-12-19 16:59:26 +0800

    v0.9.0

--- src/interpreter.py
+++ src/interpreter.py
@@ -2,11 +2,13 @@ from preprocessing import precompile
 from yacc import parser
 #from c_utils import c_utils
 from ast_ import ASTLeafNode, ASTInternalNode
+import copy
 
 class Interpreter:
     def __init__(self, src_file, dst_file):
         self.src_file = src_file
         self.dst_file = dst_file
+        self.import_cmd = None
         #print(src_file)
         if self.src_file.split('.')[-1] != 'c':
             raise Exception('The type of source file is incorrect.')
@@ -22,11 +24,123 @@ class Interpreter:
         self.import_c_utils = []
         self.import_c_utils.append('from cstdio import *')
         self.import_c_utils.append('from cstring import *')
-        self.run_cmd = '''
-        if __name__ == '__main__':
-            main_0()
-        '''
+        self.run_cmd ='\n' + "if __name__ == '__main__':" + '\n' + '    ' + 'main()'
 
+    def lint(self, s):
+        sstr = ''
+        i = 0
+        while i < len(s):
+            if s[i] == '=':
+                if s[i + 1] == '=':
+                    sstr += ' == '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' = '
+                    i = i + 1
+                    continue
+            elif s[i] == '!':
+                if s[i + 1] == '=':
+                    sstr += ' != '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += '!'
+                    i = i + 1
+                    continue
+            elif s[i] == '>':
+                if s[i + 1] == '=':
+                    sstr += ' >= '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' > '
+                    i = i + 1
+                    continue
+            elif s[i] == '<':
+                if s[i + 1] == '=':
+                    sstr += ' <= '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' < '
+                    i = i + 1
+                    continue
+            elif s[i] == '+':
+                if s[i + 1] == '=':
+                    sstr += ' += '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' + '
+                    i = i + 1
+                    continue
+            elif s[i] == '*':
+                if s[i + 1] == '=':
+                    sstr += ' *= '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' * '
+                    i = i + 1
+                    continue
+            elif s[i] == '/':
+                if s[i + 1] == '=':
+                    sstr += ' /= '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    sstr += ' / '
+                    i = i + 1
+                    continue
+            elif s[i] == '-':
+                if s[i + 1] == '=':
+                    sstr += ' -= '
+                    i = i + 2
+                    continue
+                elif s[i + 1] != '=':
+                    if not s[i + 1].isdigit():
+                        sstr += ' - '
+                        i = i + 1
+                        continue
+                    else:
+                        sstr += s[i]
+                        i = i + 1
+                        continue
+            elif s[i] == "'":
+                tmp = s.find("'", i + 1, len(s))
+                sstr += s[i:tmp + 1]
+                i = tmp + 1
+                continue
+            elif s[i] == ',':
+                sstr += ', '
+                i = i + 1
+                continue
+            elif s[i] == '"':
+                next = s.find('"', i + 1, len(s))
+                sstr += s[i:next + 1]
+                i = next + 1
+                continue
+            else:
+                sstr += s[i]
+                i = i + 1
+                continue
+        return sstr
+
+    def formatIndent(self, item, rank=-1):
+        INDENT_STRING = '    '
+        if type(item) == str:
+            item = self.lint(item)
+            # 对于只声明不定义的变量需要补上=None
+            if '(' not in item and ' ' not in item and '=' not in item and item != '' and item != 'break' \
+                    and item != 'continue' and item != 'pass' and item != 'else:' and item != 'if' and item != 'return':
+                item += ' = None'
+            return INDENT_STRING * rank + item
+        if type(item) == list:
+            lines = []
+            for i in item:
+                lines.append(self.formatIndent(i, rank + 1))
+            return '\n'.join(lines)
 
     def interpret(self):
         try:
@@ -39,11 +153,14 @@ class Interpreter:
             # TODO: semantic analysis
             raw_dst_code = self.process(tree)
             # TODO: format
+            out = self.formatIndent(raw_dst_code)
+            self.import_cmd = 'from cstdio import *' + '\n' + 'from cstring import *' + '\n'
+            out = self.import_cmd + out
             dst_code = ''
-
-            # ouput
             with open(self.dst_file, 'w+', encoding='utf-8') as f:
-                f.write(dst_code)
+                f.write(out)
+                f.write(self.run_cmd)
+
         except Exception as e:
             print(str(e))
 
@@ -165,7 +282,7 @@ class Interpreter:
 
         else:
             # 不是变量
-            if tree.key != 'IDENTIFIER':
+            if tree.ttype != 'IDENTIFIER':
                 return
             # 变量在变量表中
             if tree.value in self.variable_table.keys():
@@ -203,7 +320,7 @@ class Interpreter:
 
     def code_compose(self, tree, code_list, flag_ret):
         # 前置 ++/--
-        if tree.key == 'unary_expression' and isinstance(tree.children[0], ASTLeafNode):
+        if tree.ttype == 'unary_expression' and isinstance(tree.children[0], ASTLeafNode):
             if tree.children[0].value == '++':
                 res = [code_list[1][0] + ' = ' + code_list[1][0] + '+1']
                 """
@@ -218,7 +335,7 @@ class Interpreter:
                 return res
 
         # 后置 ++/--
-        elif tree.key == 'postfix_expression' and len(tree.children) == 2:
+        elif tree.ttype == 'postfix_expression' and len(tree.children) == 2:
             if tree.children[1].value == '++':
                 res = [code_list[0][0] + '=' + code_list[0][0] + '+1']
                 """
@@ -233,7 +350,7 @@ class Interpreter:
                 return res
 
         # return 语句
-        elif tree.key == 'jump_statement' and tree.children[0].key == 'return':
+        elif tree.ttype == 'jump_statement' and tree.children[0].ttype == 'return':
             if len(tree.children) == 2:
                 """
                 return
@@ -246,7 +363,7 @@ class Interpreter:
                 return [code_list[0][0] + ' ' + code_list[1][0]]
 
         # 选择语句
-        elif tree.key == 'selection_statement':
+        elif tree.ttype == 'selection_statement':
             if len(tree.children) == 5:
                 """
                 if 条件:
@@ -263,7 +380,7 @@ class Interpreter:
                 return ['if ' + code_list[2][0] + ':', code_list[4], 'else:', code_list[6]]
 
         # 循环语句
-        elif tree.key == 'iteration_statement':
+        elif tree.ttype == 'iteration_statement':
             #  while {
             #      ...
             #  }
@@ -286,7 +403,7 @@ class Interpreter:
                 return [code_list[2][0], 'while ' + code_list[3][0] + ':', code_list[6], code_list[4]]
 
         # 语句块的换行
-        elif tree.key == 'block_item_list':
+        elif tree.ttype == 'block_item_list':
             lst = []
             for code in code_list:
                 for c in code:
@@ -299,7 +416,7 @@ class Interpreter:
             return lst
 
         # {}作用域 去除{}
-        elif tree.key == 'compound_statement':
+        elif tree.ttype == 'compound_statement':
             if len(tree.children) == 3:
                 """
                     代码块（缩进+1）
@@ -311,10 +428,85 @@ class Interpreter:
                     pass（缩进+1）
                 """
                 return ['pass']
-        
+
+        # 函数声明, 返回值类型变为def,
+        elif tree.ttype == 'function_definition':
+            if len(tree.children) == 4:
+                pass
+            elif len(tree.children) == 3:
+                function_body = []
+                # 无脑加入所有全局变量
+                for global_var in self.global_variables:
+                    function_body.append('global ' + global_var)
+                for code in code_list[2]:
+                    function_body.append(code)
+                """
+                def 函数名:
+                    函数体（缩进+1）
+                """
+                return ['def ' + code_list[1][0] + ':',
+                        function_body]
+
+        # 函数参数列表中参数类型的去除
+        elif tree.ttype == 'parameter_declaration' and len(tree.children) == 2:
+            """
+            变量（没有类型）
+            """
+            return code_list[1]
+
+        # 函数列表中数组“[]”的去除
+        elif tree.ttype == 'direct_declarator' and len(tree.children) == 3 and tree.children[1].value == '[':
+            """
+            数组变量名
+            """
+            return [code_list[0][0]]
+
+        # 形如int x, y=2;的声明中的x, y=2 , Python中需要分行
+        elif tree.ttype == 'init_declarator_list':
+            if len(tree.children) == 1:
+                """
+                变量名（=初始值）
+                """
+                return code_list[0]
+            else:
+                """
+                变量名1（=初始值1）
+                变量名2（=初始值2）
+                """
+                return [code_list[0][0],
+                        code_list[2][0]]
+
+        # 结构体变量的声明（形如struct A a;)
+        elif tree.ttype == 'struct_or_union_specifier' and len(tree.children) == 2:
+            """
+            结构体名称
+            """
+            return code_list[1]
+
+        # 结构体的定义（形如 struct A { int x; int y; };
+        elif tree.ttype == 'struct_or_union_specifier' and len(tree.children) == 5:
+            """
+            class 结构体名称:
+                成员变量（缩进+1）
+            """
+            return [code_list[0][0] + ' ' + code_list[1][0] + ':',
+                    code_list[3]]
+
+        # 结构体定义内的语句列表
+        elif tree.ttype == 'struct_declaration_list':
+            lst = []
+            for code in code_list:
+                for c in code:
+                    lst.append(c)
+            """
+            成员变量1 = 初始值1
+            成员变量2 = 初始值2
+            ...
+            """
+            return lst
 
         # 声明语句
-        if tree.key == 'declaration' or tree.key == 'struct_declaration':
+        if tree.ttype == 'declaration' or tree.ttype == 'struct_declaration':
 
             # 变量（非结构体）声明，去除变量类型和分号
             if len(tree.children) == 3 and flag_ret == '':
@@ -350,12 +542,12 @@ class Interpreter:
         # int s[10]; == > s = [0] * 10
         # int s[5] = {1,2,3}; ==>  s = [1,2,3,0,0]
         # char s[5] = "abc"; ==>  s = ['a','b','c',0,0]
-        elif tree.key == 'direct_declarator' and len(tree.children) == 4 and \
+        elif tree.ttype == 'direct_declarator' and len(tree.children) == 4 and \
                 isinstance(tree.children[2], ASTInternalNode) and \
-                tree.children[2].key == 'assignment_expression':
+                tree.children[2].ttype == 'assignment_expression':
             return [code_list[0][0] + '=[' + 'None' + ']*' + code_list[2][0]] # 数组名 = [None] * 长度
 
-        elif tree.key == 'init_declarator' and len(tree.children) == 3 and code_list[0][0].find('[') >= 0:
+        elif tree.ttype == 'init_declarator' and len(tree.children) == 3 and code_list[0][0].find('[') >= 0:
             tmp = code_list[0][0]  # s[0]*5
             index_1 = tmp.find('[')
             left = tmp[:index_1 - 1]  # s
@@ -390,7 +582,7 @@ class Interpreter:
                 return result
 
         # 去除数组的[]
-        elif tree.key == 'initializer' and len(tree.children) == 3:
+        elif tree.ttype == 'initializer' and len(tree.children) == 3:
             return code_list[1] # 返回[]中的值
 
         # 其他情况
